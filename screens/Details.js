d5c7d99567e2e413522180a6bab96554e77a3c14
Marco Moreno <marcomorenoag@outlook.com>
2022-09-19 15:41:51 -0500

    feat: :sparkles: add project setup
    Setup for App root with Stack Navigation was added

---
+++ screens/Details.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+
+const Details = () => {
+  return (
+    <Text>Details</Text>
+  )
+}
+export default Details
---
+++ screens/Home.js
@@ -0,0 +1,8 @@
+import { Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+export default Home
